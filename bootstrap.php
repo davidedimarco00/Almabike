1b94e4feb27dcfbe1d64f653d0f8101c5802bb35
davidedima00 <davidedima00@users.noreply.github.com>
2023-03-04 22:34:51 +0100

    merge

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once("utils/functions.php");
+require_once("phpUtils/functions.php");
 require_once("db/database.php");
 
 $dbh = new DatabaseHelper("localhost", "root", "", "almabikedatabase", 3306);
