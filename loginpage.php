2b983d9adaeac02ec5790fc67e32a67196e3d559
Davide Di Marco <Davide.DiMarco@horsa.it>
2023-03-27 09:43:24 +0200

    update 20230327

--- loginpage.php
+++ loginpage.php
@@ -11,7 +11,7 @@
     $templateParams["pagereq"] = "template/loginPageTemplate.php";
 
     //require all css of the site and the specific page array contains all the css to implement
-    $templateParams["css"] = array("css/loginPage.css", "css/header.css", "css/footer.css" );  //, "css/header.css", "css/footer.css");
+    $templateParams["css"] = array("src/view/css/loginPage.css", "src/view/css/header.css", "src/view/css/footer.css" );  //, "css/header.css", "css/footer.css");
 
     require 'template/base.php';
 ?>
